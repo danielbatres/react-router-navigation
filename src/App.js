6757d44507c3b204ebe398e9fd0dbc4befa3c9f3
danielbatres <danielbatresuser@gmail.com>
2023-12-03 15:26:23 -0600

    Add menu component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { BlogPage } from "./BlogPage";
 import { ProfilePage } from "./ProfilePage";
+import { Menu } from "./Menu";
 
 function App() {
   return (
     <HashRouter>
       <Menu />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<p>Not found</p>} />
       </Routes>
+
       <footer></footer>
     </HashRouter>
   );

6e565092547ff6625dc142d8ea79bc675261d0fa
danielbatres <danielbatresuser@gmail.com>
2023-12-03 15:39:50 -0600

    Navigation with NavLink

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Menu() {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink
+            className={({ isActive }) => ""}
+            style={({ isActive }) => ({
+              color: isActive ? "red" : "blue",
+            })}
+            to="/"
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? "red" : "blue",
+            })}
+            to="/blog"
+          >
+            Blog
+          </NavLink>
         </li>
         <li>
-          <Link to="/profile">Profile</Link>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? "red" : "blue",
+            })}
+            to="/profile"
+          >
+            Profile
+          </NavLink>
         </li>
       </ul>
     </nav>
   );
 }
+
 
 export { Menu };

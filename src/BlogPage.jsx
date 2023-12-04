544534c7f0a0b149c0ec1e808ff0f9805bf87f81
danielbatres <danielbatresuser@gmail.com>
2023-12-04 09:57:33 -0600

    Add BlogLink and blogData

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function BlogPage() {
-  return <h1>BlogPage</h1>;
+  return (
+    <>
+      <h1>BlogPost</h1>
+      {blogData.map(post => (
+        <BlogLink post={post} />
+      ))}
+    </>
+  );
 }
+
+function BlogLink({ post }) {
+  return (
+    <li>
+      <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+    </li>
+  )
+}
+
+const blogData = [
+  {
+    title: "What is React?",
+    slug: "what-is-react",
+    content: "React is the best JavaScript Framework",
+    author: "user12",
+  },
+  {
+    title: "What is Vue?",
+    slug: "what-is-vue",
+    content: "Vue is the best JavaScript Framework",
+    author: "user10",
+  },
+  {
+    title: "What is Angular?",
+    slug: "what-is-angular",
+    content: "Angular is the best JavaScript Framework",
+    author: "user8",
+  }
+];
 
 export { BlogPage };

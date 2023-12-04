b3a75584e454ef44db58b36a4eff7e303215cda0
danielbatres <danielbatresuser@gmail.com>
2023-12-04 10:32:02 -0600

    Outlet: nested routes

--- src/BlogPage/index.jsx
+++ src/BlogPage/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { blogData } from "../blogdata";
 
 function BlogPage() {
   return (
     <>
       <h1>BlogPost</h1>
+      <Outlet />
       {blogData.map(post => (
         <BlogLink post={post} key={post.slug} />
       ))}

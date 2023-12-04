8560a8d823e428015895ec6d381641ce8a084bb5
danielbatres <danielbatresuser@gmail.com>
2023-12-04 10:24:14 -0600

    Using useNavigate

--- src/BlogPost/index.jsx
+++ src/BlogPost/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { blogData } from '../blogdata';
 
 function BlogPost() {
+  const navigate = useNavigate();
   const { slug } = useParams();
 
   const blogPost = blogData.find(post => post.slug === slug);
 
+  const returnToBlog = () => navigate('/blog');
+
   if (blogPost) {
     return (
       <>
+        <button onClick={returnToBlog}>Return to blog</button>
         <h2>{blogPost.title}</h2>
         <p>{blogPost.author}</p>
         <p>{blogPost.content}</p>

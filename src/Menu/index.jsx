331149cc037f44f194de4cbf3e57525e774c81b4
danielbatres <danielbatresuser@gmail.com>
2023-12-06 06:32:54 -0600

    Public and private routes

--- src/Menu/index.jsx
+++ src/Menu/index.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../auth";
 
 function Menu() {
+  const auth = useAuth(); 
+
+  const routes = [
+    { to: "/", text: "Home", private: false },
+    { to: "/blog", text: "Blog", private: false },
+    { to: "/profile", text: "Profile", private: !auth.user },
+    { to: "/login", text: "Login", private: !!auth.user },
+    { to: "/logout", text: "Logout", private: !auth.user },
+  ];
+
   return (
     <nav>
       <ul>
-        {routes.map((route) => (
-          <li key={route.to}>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "red" : "blue",
-              })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((route) => {
+          if (route.private) return null;
+
+          return (
+            <li key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "blue",
+                })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
 }
-
-const routes = [
-  { to: "/", text: "Home" },
-  { to: "/blog", text: "Blog" },
-  { to: "/profile", text: "Profile" },
-  { to: "/login", text: "Login" },
-  { to: "/logout", text: "Logout" },
-];
 
 export { Menu };
